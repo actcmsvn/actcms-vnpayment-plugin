af5b1f2b33e9133634138ab1acf8ff25c3dba99a
Trung Nguyen <actcms.com@gmail.com>
2025-03-17 09:13:02 +0700

    Update all 17032025

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Lịch sử thay đổi
+
+Tất cả thay đổi của `actcms-vietnam-bank-qr` sẽ được mô tả trong file này
+
+## 1.0.0 - 2024-11-30
+
+- Bản phát hành đầu tiên
---
+++ src/Plugin.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ACTCMS\VietnamBankQr;
+
+use Botble\PluginManagement\Abstracts\PluginOperationAbstract;
+
+class Plugin extends PluginOperationAbstract
+{
+}
